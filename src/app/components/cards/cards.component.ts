bc9db240fbdff777c586fa0e7dd57412bac04d11
Abbie Sithole <sithole@thedigitalacademy.co.za>
2021-09-28 17:36:04 +0200

    getting stats of signs table

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { BlacklistedService } from 'src/app/services/blacklisted.service';
+import { RequestedService } from 'src/app/services/requested.service';
+import { SignsService } from 'src/app/services/signs.service';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-cards',
@@ -8,9 +12,31 @@ import { Router } from '@angular/router';
 })
 export class cardsComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  public sumSigns : number = 0;
+  public sumUsers : number = 0;
+  public sumRequested : number = 0;
+  public sumBlacklisted : number = 0;
+
+
+  constructor(private router:Router, 
+    public signs:SignsService, 
+    public usersTable:UsersService, 
+    public requestedSigns:RequestedService,
+    public blacklistedUsers:BlacklistedService) { }
+
 
   ngOnInit(): void {
+
+      this.signs.getAllWord().subscribe((array)=>
+      {
+
+          //  for(let x=0; x < array.length; x++)
+          // {
+          //    this.sumSigns = array[0+1].length;
+          // }
+          this.sumSigns = Array.length;
+      })
+    
   }
 
   logout()
