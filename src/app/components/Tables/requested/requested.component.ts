e76e2b4e768a61be232fec30662744cbc36eb8e5
Abbie <79112224+AbbieS2175@users.noreply.github.com>
2021-10-01 11:31:33 +0200

    mail done

--- src/app/components/Tables/requested/requested.component.ts
+++ src/app/components/Tables/requested/requested.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from 'src/app/services/http.service';
 import { RequestedService } from 'src/app/services/requested.service';
 
 @Component({
@@ -10,7 +11,7 @@ export class RequestedComponent implements OnInit {
   
   allWords:any;
 
-  constructor(public requestedService:RequestedService) {}
+  constructor(public requestedService:RequestedService, public mail:HttpService) {}
 
   ngOnInit(): void {
 
@@ -18,6 +19,28 @@ export class RequestedComponent implements OnInit {
 
       this.allWords = words;
     });
+  }
+
+  onAccepted(word)
+  {
+    console.log(word);
+    
+    this.mail.sendEmail('http://localhost:3000/mail',     {to: "sitholeabbie@gmail.com", // list of receivers
+      subject: "Word approved", // Subject line
+      text: `Your ${word.word} is accepted`, // plaintext word
+    }).toPromise().then(() => {
+      console.log('Email sent');
+      
+    }).catch(err => {
+      console.log(err.message);
+          })
+
+
+  }
+
+  onRejected()
+  {
+
   }
 
 }
--- src/app/components/mail/mail.component.ts
+++ src/app/components/mail/mail.component.ts
@@ -13,7 +13,13 @@ export class MailComponent implements OnInit {
   loading = false;
   buttionText = "Submit";
 
-  emailFormControl = new FormControl("", [
+
+  nameFormControl = new FormControl("",[
+    Validators.required,
+    Validators.minLength(4)
+  ]);
+
+  emailFormControl = new FormControl("",[
     Validators.required,
     Validators.email
   ]);
@@ -29,23 +35,28 @@ export class MailComponent implements OnInit {
   {
     this.loading = true;
     this.buttionText = "Submiting...";
-    let user = {
+    let user = 
+    {
+      name: this.nameFormControl.value,
       email: this.emailFormControl.value
     }
-    this.http.sendEmail("http://localhost:3000/mail", user).subscribe(data => {
-        let res:any = data; 
-        console.log(`${user.email} is successfully register and mail has been sent and the message id is ${res.messageId}`
-        );
-      },
-      err => {
-        console.log(err);
-        this.loading = false;
-        this.buttionText = "Submit";
-      },() => {
-        this.loading = false;
-        this.buttionText = "Submit";
-      }
-    );
+ 
+    console.log(user);
+    
+    // this.http.sendEmail("http://localhost:3000/mail", user).subscribe(data => {
+    //     let res:any = data; 
+    //     console.log(`${user.name} 's request is successfully accepted and mail has been sent and the message id is ${res.messageId}`
+    //     );
+    //   },
+    //   err => {
+    //     console.log(err);
+    //     this.loading = false;
+    //     this.buttionText = "Submit";
+    //   },() => {
+    //     this.loading = false;
+    //     this.buttionText = "Submit";
+    //   }
+    // );
   }
 
 }
